062175409c01b134375c1bd45ed5bbb594017421
selma hadzic <selma.hadzic.se@gmail.com>
2022-03-24 17:53:04 +0100

    student js update
    added comments, updates css, added search/filter function

--- students.js
+++ students.js
@@ -1,9 +1,10 @@
 "use strict";
 
-let allStudents = DATABASE.students;
-let oneStudent = [DATABASE.students[0], DATABASE.students[1]]
+//skapar globala variablar i databasen 
+let students = DATABASE.students;
 
-
+// skapar div med id result som innehåller elevens för och efternamn samt totala credits.
+// även en div som visar repsketive elevs kurser.
 function renderStudent(id) {
     let div = document.createElement("div");
     let student = DATABASE.students[id];
@@ -22,7 +23,7 @@ function renderStudent(id) {
     return div;
 }
 
-
+// skapar en funktion som räknar ut respektive students totala credits
 function totalCredits(student) {
     let credits = []
     for (let course of student.courses) {
@@ -36,7 +37,7 @@ function totalCredits(student) {
     return creditSum;
 }
 
-function renderStudents (students) {
+function showStudents (students) {
     let studentsElement = document.getElementById("result");
     for (let student of students) {
         let studentElement = renderStudent(student.studentID);
@@ -44,6 +45,7 @@ function renderStudents (students) {
     }
 }
 
+// funktion och loop som skapar div element för studenter
 function renderCourses (student){
     let courseData = DATABASE.courses;
     let courses = [];
@@ -56,16 +58,18 @@ function renderCourses (student){
    for (let i = 0; i < courses.length; i++) {
        let div = document.createElement("div");
 
+        // delat upp vilka kurser som är klara och vilka som inte är klara - innehåller titel av kurser, när eleven började kursen,
+        // samt hur många poäng av möjliga studenten har i olika kurser 
        if (student.courses[i].passedCredits == courseData[courses[i].courseId].totalCredits) {
            let text = div.innerHTML = 
            `<div class="done"><h4>${courses[i].title}</h4>
-           <p><b>Started:</b> ${student.courses[i].started.semester} ${student.courses[i].started.year} </p>
+           <p><b><i>Started:</i></b> ${student.courses[i].started.semester} ${student.courses[i].started.year} </p>
            <p>(${student.courses[i].passedCredits} of ${courseData[courses[i].courseId].totalCredits} credits)</p></div>`
            courseDiv.push(text)
        } else {
            let text = div.innerHTML = 
            `<div class="notdone"><h4>${courses[i].title}</h4>
-           <p><b>Started:</b> ${student.courses[i].started.semester} ${student.courses[i].started.year} </p>
+           <p><b><i>Started:</b> ${student.courses[i].started.semester} ${student.courses[i].started.year} </p>
            <p>(${student.courses[i].passedCredits} of ${courseData[courses[i].courseId].totalCredits} credits)</p></div>`
            courseDiv.push(text)
        }
@@ -74,4 +78,43 @@ function renderCourses (student){
    return courseDiv.toString().split(",").join("");
 }
 
-renderStudents(DATABASE.students);
+
+
+function searchLastName() {
+    return input.value.toLowerCase();
+}
+
+let input = document.getElementById("students-search");
+input.addEventListener("keyup", studentLastName);
+
+function studentLastName (){
+    let studentsArray = []
+    for ( let i = 0; i < students.length; i++){
+        document.getElementById("result").innerHTML = ""
+        if ("" == searchLastName()){
+            document.getElementById("result").innerHTML = ""
+        } else if (students[i].lastName.toLowerCase().includes(searchLastName())) {
+            studentsArray.push(students[i]);
+        } 
+
+    }
+
+    showStudents(studentsArray)
+}
+
+function submit () {
+    let studentsArray = []
+    for ( let i = 0; i < students.length; i++){
+        if (students[i].lastName.toLowerCase().includes(searchLastName())) {
+            studentsArray.push(students[i]);
+        } 
+    }
+
+    showStudents(studentsArray)
+}
+
+input.addEventListener("submit", submit);
+
+
+// kallar (initierar) på hemsidan, alltså får fram alla studenter
+showStudents(DATABASE.students);
