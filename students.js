c08e9a6a0f54b8d0275b423802c31f5dd046e1d3
selma hadzic <selma.hadzic.se@gmail.com>
2022-03-24 12:15:20 +0100

    students update
    added courses, credits and when the student started.

--- students.js
+++ students.js
@@ -3,6 +3,7 @@
 let allStudents = DATABASE.students;
 let oneStudent = [DATABASE.students[0], DATABASE.students[1]]
 
+
 function renderStudent(id) {
     let div = document.createElement("div");
     let student = DATABASE.students[id];
@@ -10,7 +11,7 @@ function renderStudent(id) {
     div.innerHTML =
     `<header>${student.firstName} ${student.lastName} (total credits: ${totalCredits(student)}) </header>
     <div>
-        <div id="courses">
+        <div id="studentcourses">
         <h3>Courses:</h3>
         <div id="courses">
             ${renderCourses(student)}
@@ -20,6 +21,7 @@ function renderStudent(id) {
 
     return div;
 }
+
 
 function totalCredits(student) {
     let credits = []
@@ -43,12 +45,33 @@ function renderStudents (students) {
 }
 
 function renderCourses (student){
-    let courseData = DATABASE.courses
-    let courses = []
-    for ( let course of student.courses){
-        if (course.courseID == courseData.courseID)
-        courses.push(courseData)
+    let courseData = DATABASE.courses;
+    let courses = [];
+    for (let i = 0; i < student.courses.length; i++){
+        let id = student.courses[i].courseId 
+        courses.push(courseData[id])
     }
-} 
 
-renderStudents(DATABASE.students); 
+   let courseDiv = []
+   for (let i = 0; i < courses.length; i++) {
+       let div = document.createElement("div");
+
+       if (student.courses[i].passedCredits == courseData[courses[i].courseId].totalCredits) {
+           let text = div.innerHTML = 
+           `<div class="done"><h4>${courses[i].title}</h4>
+           <p><b>Started:</b> ${student.courses[i].started.semester} ${student.courses[i].started.year} </p>
+           <p>(${student.courses[i].passedCredits} of ${courseData[courses[i].courseId].totalCredits} credits)</p></div>`
+           courseDiv.push(text)
+       } else {
+           let text = div.innerHTML = 
+           `<div class="notdone"><h4>${courses[i].title}</h4>
+           <p><b>Started:</b> ${student.courses[i].started.semester} ${student.courses[i].started.year} </p>
+           <p>(${student.courses[i].passedCredits} of ${courseData[courses[i].courseId].totalCredits} credits)</p></div>`
+           courseDiv.push(text)
+       }
+   }
+
+   return courseDiv.toString().split(",").join("");
+}
+
+renderStudents(DATABASE.students);
