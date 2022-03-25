5bd5a1d60c6a035b2599d9bf92bf5d96febe8af8
selma hadzic <selma.hadzic.se@gmail.com>
2022-03-25 15:31:40 +0100

    responsible teacher

--- courses.js
+++ courses.js
@@ -10,14 +10,15 @@ function renderCourse(id) {
     `<header>${courses.title} (total credits: ${courses.totalCredits})</header>
     <div>
         <div id="studentcourses">
+
         <h3>Course Responsible:</h3>
         <div id="teachers">
-            ${courses.courseResponsible}
+        ${findResponsible(courses)}
         </div>
 
         <h3>Teachers:</h3>
+        ${findTeachers(courses)}
         <div id="teachers">
-            ${courses.teachers}
         </div>
         <h3>Students:</h3>
         <div>
@@ -36,34 +37,53 @@ function renderCourses (courses) {
     }
 }
 
-function render(student) {
-    let studentData = DATABASE.students;
-    let students = [];
-    for (let i = 0; i < student.courses.length; i++) {
-        let id = student.courses[i].courseId;
-        students.push(studentData[id])
-    }
-
-    let studentBox = []
-    for (let i = 0; i < student.length; i++) {
+function findResponsible (courses) {
+let responsibleBox = []
+        for (let i = 0; i < DATABASE.teachers.length; i++) {
         let div = document.createElement("div")
+        if (DATABASE.teachers[i].teacherId == courses.courseResponsible) {
+            let text = div.innerHTML = `
+            <div class="responsibleteacher"><h2>${DATABASE.teachers[i].firstName} ${DATABASE.teachers[i].lastName} (${DATABASE.teachers[i].post})</h2>`
+            responsibleBox.push(text);
 
-        if (student.courses[i].passedCredits == studentData[courses[i].courseId].totalCredits) {
-            let text = div.innerHTML = 
-            `<div class="done"><h2>${students[i].firstName} ${students[i].lastName} (${students[i].courseId.passedCredits})</h2>
-            <p>${student.courses[i].started.semester} ${student.courses[i].started.year}</p>`
-            studentBox.push(text);
-
-        } else {
-            let text = div.innerHTML = 
-            `<div class="notdone"><h2>${students[i].firstName} ${students[i].lastName} (${students[i].courseId.passedCredits})</h2>
-            <p>${student.courses[i].started.semester} ${student.courses[i].started.year}</p>`
-            studentBox.push(text);
-        }
+        } 
     }
-    return studentBox.toString().split(",").join("");
+    return responsibleBox.toString().split(",").join(""); 
+}     
+
+
+
+function searchCourse() {
+    return input.value.toLowerCase();
 }
 
+let input = document.getElementById("course-search");
+input.addEventListener("keyup", courseSearch);
 
+function courseSearch() {
+    let coursesArray = []
+    for (let i = 0; i < courses.length; i++){
+        document.getElementById("result").innerHTML = ""
+        if ("" == searchCourse()) {
+            document.getElementById("result").innerHTML = ""
+        } else if (courses[i].title.toLowerCase().includes(searchCourse())) {
+            coursesArray.push(courses[i]);
+        }
+    }
+
+    renderCourses(coursesArray)
+}
+
+function submit () {
+    let coursesArray = []
+    for (let i = 0; i < courses.length; i++) {
+        if (courses[i].courses.toLowerCase().includes(searchCourse)) {
+            coursesArray.push(courses[i]);
+        }
+    }
+    renderCourses(coursesArray)
+}
+
+input.addEventListener("submit", submit);
 
 renderCourses(DATABASE.courses);
